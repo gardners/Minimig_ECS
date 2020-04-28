abb30d3081658716a63975448fb1bc633a58e5ae
Paul Gardner-Stephen <paul@servalproject.org>
2020-04-28 15:03:01 +0930

    reworked to use audio-enabled HDMI generator, but audio still off

--- src/mimasa7/dvid_test.vhd
+++ src/mimasa7/dvid_test.vhd
@@ -34,24 +34,6 @@ architecture Behavioral of dvid_test is
    );
    end component;
 
-   COMPONENT dvid
-   PORT(
-      clk      : IN std_logic;
-      clk_n    : IN std_logic;
-      clk_pixel: IN std_logic;
-      red_p   : IN std_logic_vector(7 downto 0);
-      green_p : IN std_logic_vector(7 downto 0);
-      blue_p  : IN std_logic_vector(7 downto 0);
-      blank   : IN std_logic;
-      hsync   : IN std_logic;
-      vsync   : IN std_logic;          
-      red_s   : OUT std_logic;
-      green_s : OUT std_logic;
-      blue_s  : OUT std_logic;
-      clock_s : OUT std_logic
-      );
-   END COMPONENT;
-
    COMPONENT vga
    generic (
       hRez        : natural;
@@ -88,10 +70,14 @@ architecture Behavioral of dvid_test is
    signal hsync   : std_logic := '0';
    signal vsync   : std_logic := '0';
    signal blank   : std_logic := '0';
-   signal red_s   : std_logic;
-   signal green_s : std_logic;
-   signal blue_s  : std_logic;
-   signal clock_s : std_logic;
+   signal red_s   : std_logic_vector(0 downto 0);
+   signal green_s : std_logic_vector(0 downto 0);
+   signal blue_s  : std_logic_vector(0 downto 0);
+   signal clock_s : std_logic_vector(0 downto 0);
+
+   signal audio_l : std_logic_vector(15 downto 0) := x"0000";
+   signal audio_r : std_logic_vector(15 downto 0) := x"0000";
+   
 begin
    
    
@@ -104,16 +90,31 @@ clocking_inst : clocking port map (
       reset => reset
     );
 
-Inst_dvid: dvid PORT MAP(
+Inst_dvid: entity work.dvid PORT MAP(
       clk       => clk_dvi,
       clk_n     => clk_dvin, 
       clk_pixel => clk_vga,
+      clk_pixel_en => true,
+      
       red_p     => red,
       green_p   => green,
       blue_p    => blue,
       blank     => blank,
       hsync     => hsync,
       vsync     => vsync,
+
+      EnhancedMode => false,
+      IsProgressive => true,
+      IsPAL => true,
+      Is30kHz => false,
+      Limited_Range => false,
+      Widescreen => false,
+
+      HDMI_audio_L => audio_L,
+      HDMI_audio_R => audio_R,
+      HDMI_LeftEnable => false,
+      HDMI_RightEnable => false,      
+      
       -- outputs to TMDS drivers
       red_s     => red_s,
       green_s   => green_s,
@@ -121,10 +122,10 @@ Inst_dvid: dvid PORT MAP(
       clock_s   => clock_s
    );
    
-OBUFDS_blue  : OBUFDS port map ( O  => DATA_P(0), OB => DATA_N(0), I  => blue_s  );
-OBUFDS_red   : OBUFDS port map ( O  => DATA_P(1), OB => DATA_N(1), I  => green_s );
-OBUFDS_green : OBUFDS port map ( O  => DATA_P(2), OB => DATA_N(2), I  => red_s   );
-OBUFDS_clock : OBUFDS port map ( O  => CLK_P, OB => CLK_N, I  => clock_s );
+OBUFDS_blue  : OBUFDS port map ( O  => DATA_P(0), OB => DATA_N(0), I  => blue_s(0)  );
+OBUFDS_red   : OBUFDS port map ( O  => DATA_P(1), OB => DATA_N(1), I  => green_s(0) );
+OBUFDS_green : OBUFDS port map ( O  => DATA_P(2), OB => DATA_N(2), I  => red_s(0)   );
+OBUFDS_clock : OBUFDS port map ( O  => CLK_P, OB => CLK_N, I  => clock_s(0) );
     -- generic map ( IOSTANDARD => "DEFAULT")    
    
 Inst_vga: vga GENERIC MAP (
